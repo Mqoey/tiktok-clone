602401d9c7d33a2aa8c40b62955ff59e39c5918c
mqondisi <mqoeyyy@gmail.com>
2020-11-18 14:26:08 +0200

    Initial

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDey60sT4kOkq5w2PLDLauz3gk_Vg52Jdk",
+  authDomain: "tiktok-fb332.firebaseapp.com",
+  databaseURL: "https://tiktok-fb332.firebaseio.com",
+  projectId: "tiktok-fb332",
+  storageBucket: "tiktok-fb332.appspot.com",
+  messagingSenderId: "516754232568",
+  appId: "1:516754232568:web:1b7023f62437fce2e8970d",
+  measurementId: "G-5D34VLLMPR"
+};
